0030966518a184b50de013d2cc7d1cff687bf097
MegaSoul3966 <sciretta.aa.m@gmail.com>
2023-07-28 10:31:19 +1000

    Final Commit, End screen scene and script

---
+++ Adventure Game/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    public int Finish;
+    private IEnumerator EndLevel()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //When object with the Player tag enters finish goal collision and loads the main menu
+        if (other.CompareTag("pickup"))
+        {
+            StartCoroutine(EndLevel());
+            SceneManager.LoadScene(Finish);
+        }
+    }
+}
+
+
